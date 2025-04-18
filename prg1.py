1a1b2aa67e22b28be7eb0f3221af947caa8dfbeb
Shivam Shukla <shukla.shivamv@gmail.com>
2025-04-18 11:07:34 +0530

    First program done

---
+++ prg1.py
@@ -0,0 +1,10 @@
+import pandas as pd 
+# create a list
+lst1 = [100,200,300,400,500]
+# create a series from the list
+seriesd = pd.Series(lst1)
+print(seriesd)
+# display third vlaue in the series
+print(seriesd[0])
+
+
