c698fe9ca4757114ef0eaaf4fcdffa8aaaff0198
Shivam Shukla <shukla.shivamv@gmail.com>
2025-04-18 11:24:23 +0530

    What is data frame

---
+++ prg4.py
@@ -0,0 +1,8 @@
+# Data Frame
+# A data frame is like a table where the data is organized in rows and columns it is a 2-D data structure like a 2-D Array 
+# A data frame is similar to a table in sql database or a spreadsheet in excel each row represents a record with a index value on the left this index values are automatically assigned starting from zero 
+# We can create data frame by using 
+# 1. list 
+# 2. dictionary 
+# 3. from a file 
+# 4.by creating empty data frame
